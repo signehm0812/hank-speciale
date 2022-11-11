a5f32263c847b2ae10ba30d7cd728b72ab990b5a
JohanSoerensen <whs655@alumni.ku.dk>
2022-11-11 09:56:53 +0100

    two sectors with subsistence and wokring SS path

---
+++ two_sectors_subsistence/two_sector_working_SS_path/HANKmodel.py
@@ -0,0 +1,183 @@
+
+import numpy as np
+from EconModel import EconModelClass
+from GEModelTools import GEModelClass
+
+import household_problem
+import steady_state
+import blocks
+
+class HANKModelClass(EconModelClass,GEModelClass):
+    
+    #########
+    # setup #
+    #########      
+
+    def settings(self):
+        """ fundamental settings """
+
+        # a. namespaces
+        self.namespaces = ['par','ss','ini','path','sim']
+        
+        # b. household
+        self.grids_hh = ['a'] # grids
+        self.pols_hh = ['a'] # policy functions
+        self.inputs_hh = ['w_N','r','d_N','d_L','tau','Q','P'] # direct inputs
+        self.inputs_hh_z = [] # transition matrix inputs
+        self.outputs_hh = ['a','c','c_hat_N','c_N','c_L','ell','n','p'] # outputs
+        self.intertemps_hh = ['vbeg_a'] # intertemporal variables
+
+        # c. GE
+        self.shocks = ['istar','Z_N','Z_L','pm_N'] # exogenous inputs
+        self.unknowns = ['pi_N','Q','w_N','Y_L','Y_N'] # endogenous inputs
+        self.targets = ['NKPC_res_N','NKPC_res_L','clearing_A','clearing_N','clearing_C_N'] # targets
+        
+        # d. all variables
+        self.varlist = [ # all variables
+            'A',
+            'B',
+            'C',
+            'C_N',
+            'C_L',
+            'clearing_A',
+            'clearing_C',
+            'clearing_C_N',
+            'clearing_C_L',
+            'clearing_N',
+            'd',
+            'd_N',
+            'd_L',
+            'G',
+            'i',
+            'N',
+            'N_N',
+            'N_L',
+            'M_N',
+            'M_L',
+            'pm_L',
+            'pm_N',
+            'NKPC_res_N',
+            'NKPC_res_L',
+            'pi',
+            'pi_N',
+            'pi_L',                        
+            'adjcost',
+            'adjcost_N',
+            'adjcost_L',            
+            'r',
+            'istar',
+            'rstar',
+            'tau',
+            'w',
+            'w_N',
+            'w_L',            
+            'mc_N',
+            'mc_L',
+            'Y',
+            'Y_N',
+            'Y_L',
+            'Y_star',
+            'Q',
+            'P',
+            'Z_N',
+            'Z_L']
+
+        # e. functions
+        self.solve_hh_backwards = household_problem.solve_hh_backwards
+        self.block_pre = blocks.block_pre
+        self.block_post = blocks.block_post
+        
+    def setup(self):
+        """ set baseline parameters """
+
+        par = self.par
+
+        par.Nfix                = 1                                   # number of fixed discrete states (either work in L or N sector)
+        par.Nz                  = 7                                   # number of stochastic discrete states (here productivity)
+        par.r_target_ss         = 0.005
+        par.epsilon             = 0.3
+
+        # a. preferences
+        par.beta = 0.9875 # discount factor (guess, calibrated in ss)
+        par.varphi = 0.8 # disutility of labor (guess, calibrated in ss)
+        par.alpha_hh               = 0.5
+        par.gamma_hh               = 1.5                                 # Elasticity of substitution
+        par.c_bar                  = 0.1
+
+        par.sigma = 2.0 # inverse of intertemporal elasticity of substitution
+        par.nu = 2.0 # inverse Frisch elasticity
+        
+        # c. income parameters
+        par.rho_z = 0.966 # AR(1) parameter
+        par.sigma_psi = np.sqrt(0.50**2*(1-par.rho_z**2)) # std. of psi
+
+        # d. price setting
+        par.alpha_hh  = 0.25                                # weight on necessity goods above subsistence level 
+        par.alpha_L   = 0.25                                # cobb-douglas for sector L
+        par.alpha_N   = 0.75                                # cobb-douglas for sector N
+        par.gamma_L   = 0.5                                # substitution elasticity for sector L
+        par.gamma_N   = 0.5                                # substitution elasticity for sector N
+        par.mu_L      = 1.2                                # mark-up for sector L
+        par.mu_N      = 1.1                                # mark-up for sector 
+        par.kappa_L   = 0.15                               # price rigidity for sector L
+        par.kappa_N   = 0.1                               # price rigidity for sector Nmodel.
+
+        #par.M_N                 = 1.2
+        #par.M_L                 = 0.8
+        #par.M = par.M_L + par.M_N
+
+        #par.alpha               = 0.3
+        #par.gamma               = 1.1                                 # Elasticity of substitution
+        #par.mu                  = 1.2                                 # mark-up
+        #par.kappa               = 0.1                                 # slope of Phillips curve
+        #par.pm                  = 0.8
+        #par.M                   = 1.2
+
+        # e. government
+        par.phi = 1.5 # Taylor rule coefficient on inflation
+        par.phi_y = 0.0 # Taylor rule coefficient on output
+        
+        par.G_target_ss = 0.0 # government spending
+        par.B_target_ss = 5.6 # bond supply
+
+        # f. grids         
+        par.a_min = 0.0 # maximum point in grid for a
+        par.a_max = 100.0 # maximum point in grid for a
+        par.Na = 500 # number of grid points
+
+        # g. shocks
+        par.jump_Z_N      = 0.0 # initial jump
+        par.rho_Z_N       = 0.00 # AR(1) coefficeint
+        par.std_Z_N       = 0.00 # std.
+        par.jump_Z_L      = 0.0 # initial jump
+        par.rho_Z_L       = 0.00 # AR(1) coefficeint
+        par.std_Z_L       = 0.00 # std.
+        par.jump_istar  = 0.00
+        par.rho_istar   = 0.0
+        par.std_istar   = 0.0
+        par.jump_pm_N     = 0.01
+        par.rho_pm_N      = 0.9
+        par.std_pm_N      = 0.0025
+
+        # h. misc.
+        par.T = 1000 # length of path        
+        
+        par.max_iter_solve = 50_000 # maximum number of iterations when solving
+        par.max_iter_simulate = 50_000 # maximum number of iterations when simulating
+        par.max_iter_broyden = 100 # maximum number of iteration when solving eq. system
+        
+        par.tol_ss = 1e-11 # tolerance when finding steady state
+        par.tol_solve = 1e-11 # tolerance when solving
+        par.tol_simulate = 1e-11 # tolerance when simulating
+        par.tol_broyden = 1e-10 # tolerance when solving eq. system
+        
+    def allocate(self):
+        """ allocate model """
+
+        par = self.par
+
+        # b. solution
+        self.allocate_GE()
+
+    prepare_hh_ss = steady_state.prepare_hh_ss
+    find_ss = steady_state.find_ss        
---
+++ two_sectors_subsistence/two_sector_working_SS_path/blocks.py
@@ -0,0 +1,206 @@
+import numpy as np
+import numba as nb
+
+from GEModelTools import lag, lead
+   
+@nb.njit
+def block_pre(par,ini,ss,path,ncols=1):
+
+    for ncol in nb.prange(ncols):
+
+        A = path.A[ncol,:]
+        B = path.B[ncol,:]
+        C = path.C[ncol,:]
+        C_N = path.C_N[ncol,:]
+        C_L = path.C_L[ncol,:]
+        clearing_A = path.clearing_A[ncol,:]
+        clearing_C = path.clearing_C[ncol,:]
+        clearing_C_N = path.clearing_C_N[ncol,:]
+        clearing_C_L = path.clearing_C_L[ncol,:]
+        clearing_N = path.clearing_N[ncol,:]
+        d = path.d[ncol,:]
+        d_N = path.d_N[ncol,:]
+        d_L = path.d_L[ncol,:]
+        G = path.G[ncol,:]
+        i = path.i[ncol,:]
+        N = path.N[ncol,:]
+        N_N = path.N_N[ncol,:]
+        N_L = path.N_L[ncol,:]
+        M_N = path.M_N[ncol,:]
+        M_L = path.M_L[ncol,:]
+        pm_L = path.pm_L[ncol,:]
+        pm_N = path.pm_N[ncol,:]
+        NKPC_res_N = path.NKPC_res_N[ncol,:]
+        NKPC_res_L = path.NKPC_res_L[ncol,:]
+        pi = path.pi[ncol,:]
+        pi_N = path.pi_N[ncol,:]
+        pi_L = path.pi_L[ncol,:]
+        adjcost = path.adjcost[ncol,:]
+        adjcost_N = path.adjcost_N[ncol,:]
+        adjcost_L = path.adjcost_L[ncol,:]
+        r = path.r[ncol,:]
+        istar = path.istar[ncol,:]
+        rstar = path.rstar[ncol,:]
+        tau = path.tau[ncol,:]
+        w = path.w[ncol,:]
+        w_N = path.w_N[ncol,:]
+        w_L = path.w_L[ncol,:]
+        mc_N = path.mc_N[ncol,:]
+        mc_L = path.mc_L[ncol,:]
+        Y = path.Y[ncol,:]
+        Y_N = path.Y_N[ncol,:]
+        Y_L = path.Y_L[ncol,:]
+        Y_star = path.Y_star[ncol,:]
+        Q = path.Q[ncol,:]
+        P = path.P[ncol,:]
+        Z_N = path.Z_N[ncol,:]
+        Z_L = path.Z_L[ncol,:]
+        A_hh = path.A_hh[ncol,:]
+        C_hh = path.C_hh[ncol,:]
+        C_HAT_N_hh = path.C_HAT_N_hh[ncol,:]
+        C_N_hh = path.C_N_hh[ncol,:]
+        C_L_hh = path.C_L_hh[ncol,:]
+        ELL_hh = path.ELL_hh[ncol,:]
+        N_hh = path.N_hh[ncol,:]
+        P_hh = path.P_hh[ncol,:]
+
+        #################
+        # implied paths #
+        #################
+
+        # inflation
+        Q_lag = lag(ini.Q,Q)
+        pi_L[:] = (Q/Q_lag)*(1+pi_N)-1
+        pi[:] = pi_N**par.epsilon*pi_L**(1-par.epsilon) #preliminary inflation indexing
+        
+        # prices
+        P[:] = (par.alpha_hh+(1-par.alpha_hh)*Q**(1-par.gamma_hh))**(1/(1-par.gamma_hh)) # price index
+        w_L[:] = (1/Q)*w_N # real wage rate
+        pm_L[:] = (1/Q)*pm_N # real raw material price
+
+        # production
+        mc_N[:] = ((1-par.alpha_N)*(w_N/Z_N)**(1-par.gamma_N)+par.alpha_N*pm_N**(1-par.gamma_N))**(1/(1-par.gamma_N)) # marginal cost sector N
+        mc_L[:] = ((1-par.alpha_L)*(w_L/Z_L)**(1-par.gamma_L)+par.alpha_L*pm_L**(1-par.gamma_L))**(1/(1-par.gamma_L)) # marginal cost sector L
+
+        #Y_N[:] = N_N/((1-par.alpha_N)*(w_N/mc_N)**(-par.gamma_N)*Z_N**(par.gamma_N-1))
+        #Y_L[:] = N_L/((1-par.alpha_L)*(w_L/mc_L)**(-par.gamma_L)*Z_L**(par.gamma_L-1))
+
+        adjcost_N[:] = Y_N*(par.mu_N/(par.mu_N-1))*(1/(2*par.kappa_N))*(np.log(1+pi_N))**2 # adjustment cost sector N
+        adjcost_L[:] = Y_L*(par.mu_L/(par.mu_L-1))*(1/(2*par.kappa_L))*(np.log(1+pi_L))**2 # adjustment cost sector L
+
+        M_N[:] = par.alpha_N*(pm_N/mc_N)**(-par.gamma_N)*Y_N # M_N backed out from equating Y_N and M_N demand
+        M_L[:] = par.alpha_L*(pm_L/mc_L)**(-par.gamma_L)*Y_L # M_L backed out from equating Y_L and M_L demand
+
+        N_N[:] = (1-par.alpha_N)*(w_N/mc_N)**(-par.gamma_N)*Z_N**(par.gamma_N-1)*Y_N
+        N_L[:] = (1-par.alpha_L)*(w_L/mc_L)**(-par.gamma_L)*Z_L**(par.gamma_L-1)*Y_L
+
+#       Y_N[:] = (par.alpha_N**(1/par.gamma_N)*M_N**((par.gamma_N-1)/par.gamma_N)+(1-par.alpha_N)*(1/par.gamma_N)*(Z_N*N_N)**((par.gamma_N-1)/(par.gamma_N)))**(par.gamma_N/(par.gamma_N-1)) # production sector N
+        #Y_L[:] = (P*Y-Y_N)*(1/Q)
+#       Y_L[:] = (par.alpha_L**(1/par.gamma_L)*M_L**((par.gamma_L-1)/par.gamma_L)+(1-par.alpha_L)*(1/par.gamma_L)*(Z_L*N_L)**((par.gamma_L-1)/(par.gamma_L)))**(par.gamma_L/(par.gamma_L-1)) # production sector L
+        Y[:] = (Y_N+Q*Y_L)*(1/P) # overall production
+        Y_star[:] = (ss.Y_N+Q*ss.Y_L)*(1/P) # potential production
+
+        d_N[:] = Y_N-w_N*N_N-pm_N*M_N-adjcost_N # dividends sector N
+        d_L[:] = Y_L-w_L*N_L-pm_L*M_L-adjcost_L # dividends sector L
+
+        # b. monetary policy
+        #rstar[:] = par.r_target_ss
+        #istar[:] = pi + rstar
+        i[:] = istar + par.phi*pi + par.phi_y*(Y-(Y_star)) # taylor rule
+        i_lag = lag(ini.i,i)
+        r[:] = (1+i_lag)/(1+pi)-1 ## Fix these taylor rule weights 
+        #r[:] = i-pi # fisher equation
+
+        # c. government
+        B[:] = ss.B
+        tau[:] = r*B
+        G[:] = tau-r*B
+        
+        # d. aggregates
+        A[:] = ss.B
+        C_N[:] = Y_N-adjcost_N-pm_N*M_N
+        C_L[:] = Y_L-adjcost_L-pm_L*M_L
+        C[:] = (C_N + Q*C_L)/P
+        N[:] = N_N + N_L
+
+@nb.njit
+def block_post(par,ini,ss,path,ncols=1):
+
+    for ncol in nb.prange(ncols):
+
+        A = path.A[ncol,:]
+        B = path.B[ncol,:]
+        C = path.C[ncol,:]
+        C_N = path.C_N[ncol,:]
+        C_L = path.C_L[ncol,:]
+        clearing_A = path.clearing_A[ncol,:]
+        clearing_C = path.clearing_C[ncol,:]
+        clearing_C_N = path.clearing_C_N[ncol,:]
+        clearing_C_L = path.clearing_C_L[ncol,:]
+        clearing_N = path.clearing_N[ncol,:]
+        d = path.d[ncol,:]
+        d_N = path.d_N[ncol,:]
+        d_L = path.d_L[ncol,:]
+        G = path.G[ncol,:]
+        i = path.i[ncol,:]
+        N = path.N[ncol,:]
+        N_N = path.N_N[ncol,:]
+        N_L = path.N_L[ncol,:]
+        M_N = path.M_N[ncol,:]
+        M_L = path.M_L[ncol,:]
+        pm_L = path.pm_L[ncol,:]
+        pm_N = path.pm_N[ncol,:]
+        NKPC_res_N = path.NKPC_res_N[ncol,:]
+        NKPC_res_L = path.NKPC_res_L[ncol,:]
+        pi = path.pi[ncol,:]
+        pi_N = path.pi_N[ncol,:]
+        pi_L = path.pi_L[ncol,:]
+        adjcost = path.adjcost[ncol,:]
+        adjcost_N = path.adjcost_N[ncol,:]
+        adjcost_L = path.adjcost_L[ncol,:]
+        r = path.r[ncol,:]
+        istar = path.istar[ncol,:]
+        rstar = path.rstar[ncol,:]
+        tau = path.tau[ncol,:]
+        w = path.w[ncol,:]
+        w_N = path.w_N[ncol,:]
+        w_L = path.w_L[ncol,:]
+        mc_N = path.mc_N[ncol,:]
+        mc_L = path.mc_L[ncol,:]
+        Y = path.Y[ncol,:]
+        Y_N = path.Y_N[ncol,:]
+        Y_L = path.Y_L[ncol,:]
+        Y_star = path.Y_star[ncol,:]
+        Q = path.Q[ncol,:]
+        P = path.P[ncol,:]
+        Z_N = path.Z_N[ncol,:]
+        Z_L = path.Z_L[ncol,:]
+        A_hh = path.A_hh[ncol,:]
+        C_hh = path.C_hh[ncol,:]
+        C_HAT_N_hh = path.C_HAT_N_hh[ncol,:]
+        C_N_hh = path.C_N_hh[ncol,:]
+        C_L_hh = path.C_L_hh[ncol,:]
+        ELL_hh = path.ELL_hh[ncol,:]
+        N_hh = path.N_hh[ncol,:]
+        P_hh = path.P_hh[ncol,:]
+
+        #################
+        # check targets #
+        #################
+
+        # a. phillips curve
+        r_plus = lead(r,ss.r)
+        pi_N_plus = lead(pi_N,ss.pi_N)
+        pi_L_plus = lead(pi_L,ss.pi_L)
+        Y_N_plus = lead(Y_N,ss.Y_N)
+        Y_L_plus = lead(Y_L,ss.Y_L)
+
+        NKPC_res_N[:] = par.kappa_N*(mc_N-1/par.mu_N) + Y_N_plus/Y_N*np.log(1+pi_N_plus)/(1+r_plus) - np.log(1+pi_N)
+        NKPC_res_L[:] = par.kappa_L*(mc_L-1/par.mu_L) + Y_L_plus/Y_L*np.log(1+pi_L_plus)/(1+r_plus) - np.log(1+pi_L)
+
+        # b. market clearing
+        clearing_A[:] = A-A_hh
+        clearing_C_N[:] = C_N-C_N_hh
+        clearing_C_L[:] = C_L-C_L_hh
+        clearing_N[:] = N-N_hh
+        clearing_C[:] = C-C_hh
